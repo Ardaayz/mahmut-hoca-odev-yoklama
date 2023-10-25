bfa56341339ef53a405713c9d76863d363e66fa2
UgandanDUCK <ardaayaz2019@gmail.com>
2023-10-25 13:52:30 +0300

    Add project files.

---
+++ mahmut hoca odev yoklama/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace mahmut_hoca_odev_yoklama
+{
+    
+class Program
+        {
+            static void Main()
+            {
+                
+                string a = "temiz";
+                string b = "temiz";
+
+                
+                Random random = new Random();
+                if (random.Next(2) == 0)
+                {
+                    a = "kirli";
+                }
+                if (random.Next(2) == 0)
+                {
+                    b = "kirli";
+                }
+
+                
+                if (a == "kirli")
+                {
+                    Console.WriteLine("Zemin A kirli. Temizleme işlemi yapılıyor.");
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Zemin A temiz. Herhangi bir işlem yapılmıyor.");
+                }
+
+                if (b == "kirli")
+                {
+                    Console.WriteLine("Zemin B kirli. Temizleme işlemi yapılıyor.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Zemin B temiz. Herhangi bir işlem yapılmıyor.");
+                }
+            }
+        }
+
+    }
+
